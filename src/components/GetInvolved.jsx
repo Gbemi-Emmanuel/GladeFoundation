7f17563d85948f1f0652f519018fd3c8a8355f38
Emmanuel Maxify <gbemiemmanuel555@gmail.com>
2023-03-01 14:05:34 +0100

    files sent

--- src/components/GetInvolved.jsx
+++ src/components/GetInvolved.jsx
@@ -147,7 +147,7 @@ const GetInvolved = () => {
     );
   }
   return (
-    <section className="GetInvolved pt-3" id="GetInvloved">
+    <section className="GetInvolved gap-4" id="GetInvloved">
       <div className="container ">
         <div className="row headline d-flex align-items-center">
           <div className="col-md-6 left-side">
@@ -159,7 +159,7 @@ const GetInvolved = () => {
           </div>
         </div>
         <div className="row involvments">
-          <div data-aos="fade-up" data-aos-delay="300" className="col-md-4 involvementBox">
+          <div data-aos="fade-up"  data-aos-delay="300" className="col-md-4 involvementBox">
             <div className="iconBox">
               <svg height="24" viewBox="0 0 24 24" width="24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M17.726 13.02 14 16H9v-1h4.065a.5.5 0 0 0 .416-.777l-.888-1.332A1.995 1.995 0 0 0 10.93 12H3a1 1 0 0 0-1 1v6a2 2 0 0 0 2 2h9.639a3 3 0 0 0 2.258-1.024L22 13l-1.452-.484a2.998 2.998 0 0 0-2.822.504zm1.532-5.63c.451-.465.73-1.108.73-1.818s-.279-1.353-.73-1.818A2.447 2.447 0 0 0 17.494 3S16.25 2.997 15 4.286C13.75 2.997 12.506 3 12.506 3a2.45 2.45 0 0 0-1.764.753c-.451.466-.73 1.108-.73 1.818s.279 1.354.73 1.818L15 12l4.258-4.61z" /></svg>
             </div>
@@ -185,7 +185,7 @@ const GetInvolved = () => {
         </div>
         <div className="row involvmentButton pt-3">
           <div className="col-md-12">
-            <button data-aos="zoom-in-up" data-toggle="modal" data-target="#donate"><i className="fa-solid fa-heart"></i>Donate Now</button>
+            <button data-aos="zoom-in-up" className="pt-3 " data-toggle="modal" data-target="#donate"><i className="fa-solid fa-heart "></i>Donate Now</button>
           </div>
 
         </div>
